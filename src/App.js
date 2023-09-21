36a3796b609acc3dac308ce73c89a74b38e9dd86
Benard Asiimwe Bataringaya <benard.asiimwe.bataringaya@gmail.com>
2023-09-21 12:31:32 +0300

    integrate quotes for the api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { useEffect, useState } from 'react';
 import { Calculator } from './components/Calculator';
+import fetchQuotes from './logic/fetchQuotes';
+import Quote from './components/Quote';
+import ErrorComponent from './components/ErrorComponent';
 
 export default function App() {
-  return <Calculator />;
+  const initialState = {
+    loading: true, error: false, quote: '', author: '',
+  };
+  const [state, setState] = useState(initialState);
+  useEffect(() => {
+    const getResults = async () => {
+      const [result] = await (await fetchQuotes()).json();
+      if (result) {
+        setState((prevState) => ({
+          ...prevState, loading: false, error: false, quote: result.quote, author: result.author,
+        }));
+      } else {
+        setState((prevState) => ({
+          ...prevState, loading: false, error: true, quote: '', author: '',
+        }));
+      }
+    };
+    getResults();
+  }, []);
+  return (
+    <div className="wrapper">
+      <ErrorComponent loading={state.loading} error={state.error} />
+      {!state.error && <Quote quote={state.quote} author={state.author} />}
+      <Calculator />
+    </div>
+  );
 }
