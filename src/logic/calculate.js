cf26ade76e5a5ea1513925401a1c1eab0ae5840d
Benard Asiimwe Bataringaya <benard.asiimwe.bataringaya@gmail.com>
2023-09-22 09:22:52 +0300

    integrate quotes for the api (#5)

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -73,7 +73,6 @@ export default function calculate(obj, buttonName) {
         operation: null,
       };
     }
-    // '=' with no operation, nothing to do
     return {};
   }
 
@@ -86,14 +85,6 @@ export default function calculate(obj, buttonName) {
     }
     return {};
   }
-
-  // Button must be an operation
-
-  // When the user presses an operation button without having entered
-  // a number first, do nothing.
-  // if (!obj.next && !obj.total) {
-  //   return {};
-  // }
 
   // User pressed an operation after pressing '='
   if (!obj.next && obj.total && !obj.operation) {
