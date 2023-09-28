4425c6b2ecb1fda10e690a90f2fb74ae0ca71d7d
Benard Asiimwe Bataringaya <benard.asiimwe.bataringaya@gmail.com>
2023-09-28 15:16:48 +0300

    add routing

--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
-import { useEffect, useState } from 'react';
+import {
+  Route, BrowserRouter, Routes, Navigate,
+} from 'react-router-dom';
 import { Calculator } from './components/Calculator';
-import fetchQuotes from './logic/fetchQuotes';
-import Quote from './components/Quote';
-import ErrorComponent from './components/ErrorComponent';
+import FetchQuotes from './components/FetchQuotes';
+import Nav from './components/Nav';
+import Home from './components/Home';
 
-export default function App() {
-  const initialState = {
-    loading: true, error: false, quote: '', author: '',
-  };
-  const [state, setState] = useState(initialState);
-  useEffect(() => {
-    const getResults = async () => {
-      const [result] = await (await fetchQuotes()).json();
-      if (result) {
-        setState((prevState) => ({
-          ...prevState, loading: false, error: false, quote: result.quote, author: result.author,
-        }));
-      } else {
-        setState((prevState) => ({
-          ...prevState, loading: false, error: true, quote: '', author: '',
-        }));
-      }
-    };
-    getResults();
-  }, []);
+function App() {
   return (
-    <div className="wrapper">
-      <ErrorComponent loading={state.loading} error={state.error} />
-      {!state.error && <Quote quote={state.quote} author={state.author} />}
-      <Calculator />
-    </div>
+    <BrowserRouter>
+      <Nav />
+      <div className="wrapper">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quote" element={<FetchQuotes />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
+
+export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+import { NavLink } from 'react-router-dom';
+
+const Nav = () => (
+  <div className="nav-bar">
+    <nav className="nav container-width">
+      <div>
+        <h1>Math Magicians</h1>
+      </div>
+      <div className="links">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/calculator">Calculator</NavLink>
+        <NavLink to="/quote">Quotes</NavLink>
+      </div>
+    </nav>
+  </div>
+);
+
+export default Nav;
