3317d95d386b1a9e77b62e8a0827ff955f4be865
Benard Asiimwe Bataringaya <benard.asiimwe.bataringaya@gmail.com>
2023-09-29 15:26:57 +0300

    more component tests

---
+++ src/components/Home.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {render, screen} from "@testing-library/react";
+import Home from './Home';
+
+test('The home page loads correctly',()=>{
+  render(<Home />);
+  const sampleContent = screen.getByText('Welcome Math Magicians!');
+  expect(sampleContent).toBeTruthy();
+});
